b889dc299b1f055a472a012db15f0040026f49ca
Seth Ammons <seth.ammons@sendgrid.com>
2013-11-15 14:47:29 -0800

    Fix formatting. Trying to get it to run on Heroku by changing port

--- src/latencyapp/latencyapp.go
+++ src/latencyapp/latencyapp.go
@@ -1,53 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
-    "encoding/json"
 )
 
 type Message struct {
-    Message string
-    Delay int64
+	Message string
+	Delay   int64
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    var delay int64
+	var delay int64
 
 	// get the number of seconds to wait to respond
 	integerBase := 10
 	bitSize := 32
 	latency := r.FormValue("delay")
 	seconds, err := strconv.ParseInt(latency, integerBase, bitSize)
-    if err != nil {
-        seconds = 0
-    }
-    if seconds == 0  {
+	if err != nil {
+		seconds = 0
+	}
+	if seconds == 0 {
 		delay = rand.Int63n(10)
 	} else {
-        delay = seconds
-    }
+		delay = seconds
+	}
 
 	fmt.Printf("Going to wait %d seconds...\n", delay)
 
-    time.Sleep(time.Duration(delay) * time.Second)
+	time.Sleep(time.Duration(delay) * time.Second)
 
-    message := Message{"success", delay}
-    jsonMessage, err := json.Marshal(message)
-    if err != nil {
-        fmt.Println("Error marshalling to json!")
-        fmt.Println(err)
-        jsonMessage = []byte("{\"message\":\"Error marshalling json\"}")
-    }
+	message := Message{"success", delay}
+	jsonMessage, err := json.Marshal(message)
+	if err != nil {
+		fmt.Println("Error marshalling to json!")
+		fmt.Println(err)
+		jsonMessage = []byte("{\"message\":\"Error marshalling json\"}")
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprintf(w, string(jsonMessage[:]))
 }
 
 func main() {
-	http.HandleFunc("/", handler)              // redirect all urls to the handler function
-	http.ListenAndServe("localhost:9999", nil) // listen for connections at port 9999 on the local machine
+	http.HandleFunc("/", handler)                   // redirect all urls to the handler function
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil) // listen for connections at port 9999 on the local machine
 }
