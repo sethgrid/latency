9b4af73b1ce895c1f9520252fcd7fb45196dfb4a
Seth Ammons <seth.ammons@sendgrid.com>
2013-11-16 15:43:24 -0800

    changed sample page to return json

--- src/latencyapp/latencyapp.go
+++ src/latencyapp/latencyapp.go
@@ -3,18 +3,26 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/nu7hatch/gouuid"
 	"log"
 	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
 	"time"
-    "github.com/nu7hatch/gouuid"
 )
 
 type Message struct {
 	Message string `json:"message"`
 	Delay   int64  `json:"delay"`
+}
+
+type Url struct {
+	Url string `json:"url"`
+}
+
+type UrlList struct {
+	Urls []Url `json:"urls"`
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -56,16 +64,20 @@ func sampleHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Type", "application/json")
 
+	urlList := UrlList{}
 	for i := 0; i < numUrls; i++ {
-        u, err := uuid.NewV4()
-        if err != nil{
-            fmt.Println("Error with uuid")
-            u = nil
-        }
-		fmt.Fprintf(w, "http://%s/%s\n", r.Host, u)
+		u, err := uuid.NewV4()
+		if err != nil {
+			fmt.Println("Error with uuid")
+			u = nil
+		}
+		url := Url{Url: fmt.Sprintf("http://%s/%s", r.Host, u)}
+		urlList.Urls = append(urlList.Urls, url)
 	}
+	jsonMessage, _ := json.Marshal(urlList)
+	fmt.Fprintf(w, "%s", jsonMessage)
 }
 
 func main() {
